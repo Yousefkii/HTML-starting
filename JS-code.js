ccc072a9bd537e81c695f820882a43525a18354a
Yousefkii <youssefhegaze908@gmail.com>
2024-12-26 16:52:54 +0100

    Add files via upload

---
+++ JS-code.js
@@ -0,0 +1,33 @@
+function globaly()
+ {
+  let sending_buttons = document.querySelector('#sending_buttons');
+   sending_buttons.setAttribute("hidden","");
+  let showing = document.querySelector("#globaly_det");
+   showing.removeAttribute("hidden");
+}
+
+let recovering = document.append(showing);
+
+function domicticly()
+{
+  let sending_buttons = document.querySelector('#sending_buttons');
+  sending_buttons.setAttribute("hidden","");
+ let showing = document.querySelector("#globaly_det");
+  showing.removeAttribute("hidden");
+}
+function package()
+{
+
+}
+function documents()
+{
+
+}
+function butt(){
+  let pp = document.createElement("p");
+  pp.className="pp";
+  let cont = document.createTextNode("this is a very paragraph");
+  pp.appendChild(cont);
+  let divo = document.querySelector("#buttooon");
+  divo.appendChild(pp);
+}
