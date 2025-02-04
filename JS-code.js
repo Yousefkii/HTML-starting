1b17dacbe7bb79150d24489cfa1ca128a1f45f73
Yousefkii <youssefhegaze908@gmail.com>
2025-02-04 01:37:36 +0100

    news section logic

--- JS-code.js
+++ JS-code.js
@@ -267,6 +267,54 @@ if (document.querySelector(".docs_numbers" )) {
       }
     })
 }
+function biggest_crank(){
+  document.querySelector("#news_pic").style.backgroundImage = "url('https://d3f3h35mrzf979.cloudfront.net/Pictures/480xany/3/2/1/81321_wettering.JPG')";
+  let cran_position = document.querySelector("#cran");
+  cran_position.style.marginLeft ="120px";
+  cran_position.style.fontWeight ="bolder";
+  cran_position.style.transitionDuration ="500ms";
+  document.querySelector("#pack").style.color ="gray";
+  document.querySelector("#air").style.color ="gray";
+
+  document.querySelector("#cran").addEventListener("mouseleave", ()=>{
+    cran_position.style.marginLeft ="85px"
+    cran_position.style.fontWeight ="normal";
+    document.querySelector("#pack").style.color ="black";
+  document.querySelector("#air").style.color ="black";
+  });
+}
+
+function packages_trans(){
+  let crank = document.querySelector("#news_pic").style.backgroundImage = "URL('https://effectshippingllc.com/wp-content/uploads/2021/12/sdm1.jpg')";
+  let cran_position = document.querySelector("#pack");
+  cran_position.style.marginLeft ="120px";
+  cran_position.style.fontWeight ="bolder";
+  cran_position.style.transitionDuration ="500ms";
+  document.querySelector("#cran").style.color ="gray";
+  document.querySelector("#air").style.color ="gray";
+
+  document.querySelector("#pack").addEventListener("mouseleave", ()=>{
+    cran_position.style.marginLeft ="85px"
+    cran_position.style.fontWeight ="normal";
+    document.querySelector("#cran").style.color ="black";
+  document.querySelector("#air").style.color ="black";
+  });
+}
+function airplanes(){
+  let crank = document.querySelector("#news_pic").style.backgroundImage = "URL('https://img.etimg.com/thumb/width-1600,height-900,imgsize-143180,resizemode-75,msid-113334097/news/international/us/passengers-board-airplanes-from-left-side-heres-why.jpg')";
+  let cran_position = document.querySelector("#air");
+  cran_position.style.marginLeft ="120px";
+  cran_position.style.fontWeight ="bolder";
+  cran_position.style.transitionDuration ="500ms";
+  document.querySelector("#pack").style.color ="gray";
+  document.querySelector("#cran").style.color ="gray";
+
+  document.querySelector("#air").addEventListener("mouseleave", ()=>{
+    cran_position.style.marginLeft ="85px"
+    cran_position.style.fontWeight ="normal";
+    document.querySelector("#pack").style.color ="black";
+  document.querySelector("#cran").style.color ="black";
+  });
 function scrol (){
   window.scrollTo({
     left :0,
